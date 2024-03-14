7db8b4b27c619831df6f69975eb09ae4426a9e3d
Nahuel Anselmo <anselmonahuel63t@gmail.com>
2024-03-14 16:22:37 -0300

    Tercer ejercicio

--- tp-1/js/ejercicios.js
+++ tp-1/js/ejercicios.js
@@ -1,4 +1,5 @@
 alert("hola gente linda"); // ejercicio 1
 
-document.write("Hello Word!! uwu"); // ejercicio 2
+document.write("Hello Word!! uwu <br>"); // ejercicio 2
 
+document.write("3 + 5 = ", 3+5, "<br>"); // ejercicio 3
